e279b25fec31be802fa5d9fd77c974cfadfa54f1
viethq <viethq@viethq6s-imac.local>
2018-11-01 10:59:29 +0700

    Initial Commit

---
+++ MEStyleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEStyleView
+//
+//  Created by viethq on 11/1/18.
+//  Copyright © 2018 viethq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MEStyleViewTests/MEStyleViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  MEStyleViewTests.swift
+//  MEStyleViewTests
+//
+//  Created by viethq on 11/1/18.
+//  Copyright © 2018 viethq. All rights reserved.
+//
+
+import XCTest
+@testable import MEStyleView
+
+class MEStyleViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
